df1aff8d2f8e1fc3b72a01e02599cd34226f151c
ahtiirs <78646730+ahtiirs@users.noreply.github.com>
2021-11-16 21:02:38 +0200

    user hash working

--- src/db.ts
+++ src/db.ts
@@ -2,6 +2,7 @@ import course from './components/courses/interfaces';
 import group from './components/groups/interfaces';
 import teacher from './components/teachers/interfaces';
 import homework from './components/homeworks/interfaces';
+import { User } from './components/users/interfaces';
 
 /**
 * Database interface
@@ -11,13 +12,32 @@ import homework from './components/homeworks/interfaces';
     courses: course[];
     teachers: teacher[];
     homeworks: homework[];
+    users: User[];
   
   }
 /**
 * Mock database
 */
 const db: Db = {
-    groups: [
+  users: [
+    {
+      id: 1,
+      firstName: 'Juku',
+      lastName: 'Juurikas',
+      email: 'juku@juurikas.ee',
+      password: '$2b$10$nevnzRS0jBjFh.KEYSoQ6u75M7FdLA7vXEgbbV9iHfU7W/.6W9hFa',
+      role: 'Admin',
+    },
+    {
+      id: 2,
+      firstName: 'Mari',
+      lastName: 'Maasikas',
+      email: 'mari@maasikas.ee',
+      password: '$2b$10$FY6sDSftFysyuOVqIpgc..qr./DoTxFCUUZALvqXUF98nz8/wlNeO',
+      role: 'User',
+    },
+  ],
+  groups: [
       {
         id: 1,
         Name: "RIF1",
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,9 @@ import coursesRouter from './components/courses/routes';
 import homeworksRouter from './components/homeworks/routes';
 import teachersRouter from './components/teachers/routes';
 import pingRouter from './components/ping/routes';
+import usersController from './components/users/controller';
+import authController from './components/auth/controller';
+
 import myCookie from './cookies'
 const cookieParser = require('cookie-parser');
 const app: Application = express();
@@ -17,12 +20,29 @@ app.use(cookieParser())
 // pordi nr mida api kuulab 
 const port = 3000; 
 
+
+/**
+ * *********************** Login ******************
+ */
+ app.post('/login', authController.login);
+
+ 
+
 app.use(myCookie);
 app.use('/ping', pingRouter);
 app.use('/groups', groupsRouter);
 app.use('/courses', coursesRouter);
 app.use('/homeworks', homeworksRouter);
 app.use('/teachers', teachersRouter);
+
+/**
+ * *********************** Users ******************
+ */
+ app.get('/users', usersController.getAllUsers);
+ app.get('/users/:id', usersController.getUserById);
+ app.delete('/users/:id', usersController.removeUser);
+ app.post('/users', usersController.createUser);
+ app.patch('/users/:id', usersController.updateUser);
 
 /**
 * Start listening
